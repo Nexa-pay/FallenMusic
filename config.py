641d896eb870973c365fb59429efd9815cd34fc1
Nexa-pay <stellargamingg99@gmail.com>
2025-12-25 03:30:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ SESSION = getenv("SESSION", None)
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tunemusic_dot")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PolicyPixels")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8276604094").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
