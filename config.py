70eb2d6fe3a7bc4c91d805353b0ace876bb538fd
Nexa-pay <stellargamingg99@gmail.com>
2025-12-25 03:16:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = getenv("3967519b70936b859949e093f873141a")
 BOT_TOKEN = getenv("BOT_TOKEN", 8276604094:AAHJwN7-6jw4_ZaL25PKMD6nMQhUEt0cnMs)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("8264803525"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
